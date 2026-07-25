ddf5a439dfd32b3efe9c08d765d4cd97efe47ef9
navneet <navneet23341@iiitd.ac.in>
2026-07-25 05:47:49 +0530

    fixed the bug of scrolling motion

--- Aestra-frontend/src/components/OutfitRow.jsx
+++ Aestra-frontend/src/components/OutfitRow.jsx
@@ -1,63 +1,38 @@
-import { useEffect, useRef } from "react";
-import "./OutfitRow.css";
+import { motion } from "motion/react";
 import OutfitCard from "./OutfitCard";
+import "./OutfitRow.css";
 import temp from "../assets/temp.jpg";
 
 const outfits = [
   temp, temp, temp, temp,
-  temp, temp, temp, temp
+  temp, temp, temp, temp,
 ];
 
 export default function OutfitRow({ direction }) {
 
-  const marqueeRef = useRef(null);
-
-  useEffect(() => {
-
-    const marquee = marqueeRef.current;
-
-    let position = 0;
-
-    const speed = 0.6;
-
-    const trackWidth = marquee.scrollWidth / 2;
-
-    function animate() {
-
-      if (direction === "left") {
-
-        position -= speed;
-
-        if (Math.abs(position) >= trackWidth)
-          position = 0;
-
-      } else {
-
-        position += speed;
-
-        if (position >= 0)
-          position = -trackWidth;
-
-      }
-
-      marquee.style.transform = `translateX(${position}px)`;
-
-      requestAnimationFrame(animate);
-    }
-
-    const id = requestAnimationFrame(animate);
-
-    return () => cancelAnimationFrame(id);
-
-  }, [direction]);
+  const loop = [...outfits, ...outfits, ...outfits, ...outfits];
 
   return (
 
     <div className={`slider ${direction}`}>
 
-      <div className="marquee" ref={marqueeRef}>
+      <motion.div
+        className="track"
 
-        {[...outfits, ...outfits].map((img, i) => (
+        animate={{
+          x: direction === "left"
+            ? ["0%", "-50%"]
+            : ["-50%", "0%"],
+        }}
+
+        transition={{
+          duration: 25,
+          ease: "linear",
+          repeat: Infinity,
+        }}
+      >
+
+        {loop.map((img, i) => (
 
           <OutfitCard
             key={i}
@@ -66,10 +41,9 @@ export default function OutfitRow({ direction }) {
 
         ))}
 
-      </div>
+      </motion.div>
 
     </div>
 
   );
-
 }
